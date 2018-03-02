f68ba202bde673282b3cab13e84cdd9b5d9de88b
samuel <samuel.mathes@gmx.de>
2018-03-02 21:44:11 +0100

    Styling

--- js/Letter.js
+++ js/Letter.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var autosize = require("autosize")
     var date = new Date();
     var _fieldset = $("<fieldset>");
     var _select = $("<select>", {"name": "sender", "id": "sender"});
@@ -8,7 +9,8 @@ $(document).ready(function () {
     var _address = $("<div>", {"id":"address", "html":"Max Mustermann<br>Musterstraße 12<br>12345 Musterstadt"})
     var _dateField = $("<div>", {"id":"date", "html":"Reutlingen, den <input type='text' id='datepicker'>"})
     var _subject = $("<div>", {"id":"subject", "html": "Betreff: Ihr Schreiben vom"})
-    var _text = $("<div>", {"id":"text", "html": "Lorem ipsum"})
+    var _text = $("<textarea>", {"id": "text"}).val("Sehr geehrter Herr \n\nLorem Ipsum");
+    autosize(_text)
     var _greeting = $("<div>", {"id": "greeting", "html": "Grüße <br><br>Samuel Mathes"});
 
     _address.prepend(_fieldset);
