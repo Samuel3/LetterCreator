bacc371edb40a0f3714021be5cba7bc5e754a0bb
samuel <samuel.mathes@gmx.de>
2018-05-10 21:48:56 +0200

    Changed order of icons and npm target

--- build/prebuild.js
+++ build/prebuild.js
@@ -1,6 +1,7 @@
 /**
  * Created by Samuel on 30.03.2018.
  */
+const fs = require("fs")
 var mainBuildFile = JSON.parse(fs.readFileSync("package.json"));
 var minorBuildVersion = JSON.parse(fs.readFileSync("app/package.json")).version;
 
