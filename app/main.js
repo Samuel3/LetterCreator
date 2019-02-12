697c308ab1db7b2fd6f4b3676b8e060de9514bad
samuel <samuel.mathes@gmx.de>
2019-02-12 20:51:47 +0100

    Modified preview dialog.

--- app/main.js
+++ app/main.js
@@ -36,7 +36,9 @@ ipcMain.on('print-to-pdf', function (event) {
 ipcMain.on('print', function (event) {
     const win = BrowserWindow.fromWebContents(event.sender)
     win.webContents.print({pageSize: "A4"}, function (error, data) {
-        if (error) throw error
+        if (error) {
+            throw error
+        }
     })
 });
 
